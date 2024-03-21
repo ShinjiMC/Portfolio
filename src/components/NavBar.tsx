ab426ee2ce8bd50d675ba4251afa3dd9d57f8950
ShinjiMC <nayapbrau@gmail.com>
2024-03-21 13:07:57 -0500

    Fix NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,58 +1,68 @@
 import React, { useState } from "react";
-import logo from "../assets/logo.svg";
-import menuIcon from "../assets/menu.svg";
-import closeIcon from "../assets/close.svg";
-import { BrowserRouter, Link } from "react-router-dom";
+import { Link, BrowserRouter } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import LanguageSelector from "./i18n/LanguageSelector";
+import { Logo, MenuOpenIcon, MenuCloseIcon } from "./icons";
 import styles from "../scss/NavBar.module.scss";
 import "./i18n";
 
 const NavBar: React.FC = () => {
   const { t } = useTranslation();
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
 
   return (
-    <header className={styles.navBar}>
-      <label className={styles.icon} onClick={() => setIsMenuOpen(!isMenuOpen)}>
-        {isMenuOpen ? (
-          <img src={closeIcon} alt="Cerrar" />
-        ) : (
-          <img src={menuIcon} alt="Menú" />
-        )}
-      </label>
+    <>
+      <nav className={styles.navbar}>
+        <div className={styles.selector}>
+          <LanguageSelector />
+        </div>
+        <div className={styles.nav_container}>
+          <span>Shinji</span>
+          <span className={styles.icon}>
+            <Logo color="white" height="60" />
+          </span>
 
-      <div className={styles.logoContainer}>
-        <img src={logo} alt="Logo" className={styles.logo} />
-      </div>
-
-      <nav
-        className={`${styles.menuContainer} ${isMenuOpen ? styles.show : ""}`}
-      >
-        <BrowserRouter>
-          <ul className={styles.menu}>
-            <li>
-              <Link to="/" onClick={() => setIsMenuOpen(false)}>
-                {t("NavBar.Home")}
-              </Link>
-            </li>
-            <li>
-              <Link to="/about" onClick={() => setIsMenuOpen(false)}>
-                {t("NavBar.About")}
-              </Link>
-            </li>
-            <li>
-              <Link to="/contact" onClick={() => setIsMenuOpen(false)}>
-                {t("NavBar.Contact")}
-              </Link>
-            </li>
+          <ul
+            className={
+              click ? `${styles.nav_menu} ${styles.active}` : styles.nav_menu
+            }
+          >
+            <BrowserRouter>
+              <li className={styles.nav_item}>
+                <Link to="/" onClick={handleClick}>
+                  {t("NavBar.Home")}
+                </Link>
+              </li>
+              <li className={styles.nav_item}>
+                <Link to="/about" onClick={handleClick}>
+                  {t("NavBar.About")}
+                </Link>
+              </li>
+              <li className={styles.nav_item}>
+                <Link to="/contact" onClick={handleClick}>
+                  {t("NavBar.Contact")}
+                </Link>
+              </li>
+              <li className={`${styles.nav_item} ${styles.disabled}`}>
+                <LanguageSelector />
+              </li>
+            </BrowserRouter>
           </ul>
-        </BrowserRouter>
+          <div className={styles.nav_icon} onClick={handleClick}>
+            {click ? (
+              <span className={styles.icon}>
+                <MenuCloseIcon color="white" height="30" />
+              </span>
+            ) : (
+              <span className={styles.icon}>
+                <MenuOpenIcon color="white" height="30" />
+              </span>
+            )}
+          </div>
+        </div>
       </nav>
-      <div className={styles.languageSelectorContainer}>
-        <LanguageSelector />
-      </div>
-    </header>
+    </>
   );
 };
 
