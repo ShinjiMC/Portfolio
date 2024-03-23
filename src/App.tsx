86d1609e85d3299a3aea10b36a95e9ccb2b717c8
ShinjiMC <nayapbrau@gmail.com>
2024-03-23 13:06:50 -0500

    Add TabButton and About

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
+import About from "./components/About";
 
 function App() {
   return (
     <>
       <NavBar />
       <Hero />
+      <About />
     </>
   );
 }
---
+++ src/components/TabButton.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { motion } from "framer-motion";
+import styles from "../scss/TabButton.module.scss";
+
+interface TabButtonProps {
+  active: boolean;
+  selectTab: () => void;
+  children: React.ReactNode;
+}
+
+const variants = {
+  default: { width: 0 },
+  active: { width: "calc(100% - 0.75rem)" },
+};
+
+const TabButton: React.FC<TabButtonProps> = ({
+  active,
+  selectTab,
+  children,
+}) => {
+  const buttonClasses = active ? styles.textWhite : styles.textADB7BE;
+
+  return (
+    <button onClick={selectTab}>
+      <p className={`mr-3 font-semibold hover:text-white ${buttonClasses}`}>
+        {children}
+      </p>
+      <motion.div
+        animate={active ? "active" : "default"}
+        variants={variants}
+        className={styles.motion}
+      ></motion.div>
+    </button>
+  );
+};
+
+export default TabButton;
